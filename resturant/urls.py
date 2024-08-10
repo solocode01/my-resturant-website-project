4f9c9f21a20160e934fc4f71c86631103bd4c944
solocode01 <140919255+solocode01@users.noreply.github.com>
2024-08-10 18:47:31 +0100

    Update urls.py

--- resturant/urls.py
+++ resturant/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('menu.urls')),
+    path('/', include('menu.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
