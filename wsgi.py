f697bc77ad285d36be74e6ba736bdfe48bbb1b97
solocode01 <140919255+solocode01@users.noreply.github.com>
2024-08-22 01:52:29 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application('PORT', '10000')
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resturant.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application('PORT', '10000')
