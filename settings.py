e4c202e47d269bc98a997dd0568f146ecfacdfed
solocode01 <140919255+solocode01@users.noreply.github.com>
2024-08-22 01:42:47 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'resturant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_new',
+        'USER': 'root',
+        'PASSWORD': 'SolomonK@1',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
